7fa4266c2fca4e3c8f583c144dc16b543c1c944c
mac <mac@macui-MacBookAir.local>
2024-01-02 13:38:56 +0900

    post 요청

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const pool = mysql2.createPool({
 });
 
 const app = express()
+app.use(express.json());
 const port = 3000
 
 const wiseSayings = [
@@ -34,6 +35,38 @@ app.get("/wise-sayings", async (req, res) => {
   res.json(rows);
 });
 
+app.post("/wise-sayings", async (req, res) => {
+  const { author, content } = req.body;
+
+  if(!author) {
+    res.status(400).json({
+      msg: "author required"
+    });
+    return;
+  }
+  
+  if(!content ) {
+    res.status(400).json({
+      msg: "content required"
+    });
+    return;
+  }
+  
+  const [rs] = await pool.query(
+  `
+  INSERT INTO wise_saying
+  SET reg_date = NOW(),
+  content = ?,
+  author = ?
+  `,
+  [content, author]
+  );
+
+  res.status(201).json({
+    id: rs.insertId,
+  });
+});
+
 
 app.get("/wise-sayings/:id", async (req, res) => {
   const { id } = req.params
@@ -52,5 +85,5 @@ app.get("/wise-sayings/:id", async (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Examㅎple app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
